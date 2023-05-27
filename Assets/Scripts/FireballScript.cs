4933ca54a7d927da4d1dd5a3695563c309c3bd71
sid6595 <ksmallareddygari@gmail.com>
2023-05-26 21:24:28 -0400

    Add files via upload

---
+++ Assets/Scripts/FireballScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballScript : MonoBehaviour
+{
+
+    public GameObject explosionVFX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("Triggered by: " + collider.gameObject.name);
+        Destroy(gameObject);
+        Destroy(Instantiate(explosionVFX, transform.position, transform.rotation), 1);
+    }
+}
+ 
